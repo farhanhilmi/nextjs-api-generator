b05f4a97125c93b2d9243f6c3f2a17d430a7bff2
Farhan Hilmi <farhanhilmi077@gmail.com>
2023-04-14 15:37:47 +0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,20 @@ import Footer from '@/components/footer';
 import Contact from '@/components/home/contact';
 import Features from '@/components/home/feature';
 import Hero from '@/components/home/hero';
+import Head from 'next/head';
 
 export default function Home() {
     return (
-        <div className="min-h-screen">
-            <Hero />
-            <Features />
-            <Contact />
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <title>API Generator</title>
+            </Head>
+            <div className="min-h-screen">
+                <Hero />
+                <Features />
+                <Contact />
+                <Footer />
+            </div>
+        </>
     );
 }
